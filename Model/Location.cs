184e4bbcc4a05054a08c1ca8dc7157a12a33dd4d
Muhaimin Khamsan <muhaiminkhamsan@gmail.com>
2024-03-30 23:36:42 +0800

    Weather forecast view, Model and external API connection services

---
+++ Model/Location.cs
@@ -0,0 +1,8 @@
+﻿namespace WeatherForecastUI.Model
+{
+    public class Location
+    {
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+    }
+}
---
+++ Model/Weather.cs
@@ -0,0 +1,12 @@
+﻿namespace WeatherForecastUI.Model
+{
+    public class Weather
+    {
+        public DateTime Date { get; set; }
+        public double Temperature {  get; set; }
+        public string Summary { get; set; }
+
+        public bool Success { get; set; }
+        public string Message { get; set; }
+    }
+}
